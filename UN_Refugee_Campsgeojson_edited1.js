dd251967a0f2b972d028c6500a233bbabb06e6dd
tcurry4 <tcurry4@gmu.edu>
2016-06-03 18:11:43 -0400

    Add files via upload

---
+++ UN_Refugee_Campsgeojson_edited1.js
@@ -0,0 +1,103 @@
+var bicycleRental = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    41.221,
+                    34.418
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Al Obaidy"
+            },
+            "id": 2
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    45.610139,
+                    35.375216
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Arbat Camp"
+            },
+            "id": 3
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    44.34,
+                    36.498
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Basirma"
+            },
+            "id": 4
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    43.889,
+                    36.47
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Darashakran"
+            },
+            "id": 5
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    42.88,
+                    36.78
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Domiz 1"
+            },
+            "id": 6
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    42.895752,
+                    36.763926
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Domiz 2"
+            },
+            "id": 7
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    43.608,
+                    36.343
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Gawilan"
+            },
+            "id": 8
+        }
+    ]
+};
